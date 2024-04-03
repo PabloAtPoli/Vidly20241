1e6ed275749830f54513ba8b23663a55a3e057d8
Pablo Ortiz <paortiz@elpoli.edu.co>
2024-04-03 09:48:46 -0500

    I added to the admin interface the search box and filter to the movie list and aggregated count movies field  in the Genre list

--- Movies/admin.py
+++ Movies/admin.py
@@ -1,16 +1,27 @@
 from django.contrib import admin
+from django.db.models import Count
 from .models import Genre, Movie
 
 class GenreAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
+    list_display = ('id', 'name', 'count_movies')
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(_count_movies=Count("movie"))  # Annotate the count of movies
+        return queryset
+
+    def count_movies(self, obj):
+        return obj._count_movies
+    count_movies.admin_order_field = '_count_movies'  # Make count_movies sortable
+    count_movies.short_description = 'Count Movies'
 
 class MovieAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'release_year', 'number_in_stock', 'daily_rate', 'genre', 'date_created')
     exclude = ('date_created',)
+    search_fields = ['title', 'release_year']
+    list_filter = ('genre',)
 
 # Register your models here.
-
-
 
 admin.site.site_header = "Vidly Administration"
 admin.site.site_title = "Vidly Administration"
@@ -18,4 +29,3 @@ admin.site.index_title = "Welcome to Vidly Administration"
 
 admin.site.register(Genre, GenreAdmin)
 admin.site.register(Movie, MovieAdmin)
-
